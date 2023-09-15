3d5eb3e9eb311d1c6fd3d5e38dc36737621d295a
Tomáš Kelbasa <tomas.plqa@gmail.com>
2023-09-15 17:00:23 +0200

    addToListBtn fixes

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-if (window.location.href.toLowerCase().includes('product')) {
+if (window.location.href.toLowerCase().includes('product') && document.querySelector(".addToListBtn")) {
 	let btn = document.createElement('button');
 	btn.textContent = 'add to list';
 	btn.classList.add('addToListBtn');
@@ -7,16 +7,14 @@ if (window.location.href.toLowerCase().includes('product')) {
 }
 
 window.addEventListener('click', function (event) {
-    
 	// Tato funkce se spustí při změně URL v historii prohlížeče
-	if (window.location.href.toLowerCase().includes('product')) {
-		let btn = document.createElement('button');
-		btn.textContent = 'add to list';
-		btn.classList.add('addToListBtn');
-
-		document.querySelector('#productDetail').appendChild(btn);
-		const newHash = window.location.hash;
-		console.log('Nový hash:', newHash);
+	if (window.location.href.toLowerCase().includes('item')) {
+		setTimeout(function () {
+			let btn = document.createElement('button');
+			btn.textContent = 'add to list';
+			btn.classList.add('addToListBtn');
+			document.querySelector('#productDetail')?.appendChild(btn);
+		}, 1000);
 	}
 
 	// Zde můžete volat svou funkci nebo provádět další akce
--- main.js
+++ main.js
@@ -8,50 +8,55 @@ function generateProduct(url, ix) {
 					data.split('<body>')[1],
 					'text/html'
 				);
-				let priceInput = dc.querySelector('[data-test="product-price"');
-				if (priceInput.querySelector('span') !== null) {
-					priceInput.removeChild(priceInput.querySelector('span'));
-				}
-
-				let nameInput = dc.querySelector('h2');
-
-				let detailInput = dc.querySelector('.detailQuantity');
-
-				let newDiv = document.createElement('div');
-				newDiv.classList.add('product-box');
-				newDiv.setAttribute('product-index', ix);
-
-				let productName = document.createElement('a');
-				productName.className = 'product-name';
-				productName.href = url;
-				productName.target = '_blank';
-				newDiv.appendChild(productName);
-				newDiv.querySelector('.product-name').textContent =
-					nameInput.textContent + ' - ' + detailInput?.textContent;
-
-				let priceBox = document.createElement('div');
-				priceBox.classList.add("product-priceBox");
-				let productPrice = document.createElement('p');
-				productPrice.className = 'product-price';
-				productPrice.textContent = priceInput.textContent;
-				priceBox.appendChild(productPrice);
-
-
-				let removeButton = document.createElement('button');
-				removeButton.classList.add('product-removeBtn');
-				removeButton.textContent = '-';
-				removeButton.addEventListener('click', (ab) => {
-					const elementToRemove = document.querySelector(
-						`[product-index="${ix}"]`
-					);
-					if (elementToRemove) {
-						elementToRemove.remove();
+				if (
+					dc.querySelector('[data-test="product-price"') &&
+					dc.querySelector('h2') &&
+					dc.querySelector('.detailQuantity')
+				) {
+					let priceInput = dc.querySelector('[data-test="product-price"');
+					if (priceInput.querySelector('span') !== null) {
+						priceInput.removeChild(priceInput.querySelector('span'));
 					}
-					urlList[ix] = null;
-				});
-				priceBox.appendChild(removeButton);
-				newDiv.appendChild(priceBox);
-				document.querySelector('.prices').appendChild(newDiv);
+
+					let nameInput = dc.querySelector('h2');
+
+					let detailInput = dc.querySelector('.detailQuantity');
+
+					let newDiv = document.createElement('div');
+					newDiv.classList.add('product-box');
+					newDiv.setAttribute('product-index', ix);
+
+					let productName = document.createElement('a');
+					productName.className = 'product-name';
+					productName.href = url;
+					productName.target = '_blank';
+					newDiv.appendChild(productName);
+					newDiv.querySelector('.product-name').textContent =
+						nameInput.textContent + ' - ' + detailInput?.textContent;
+
+					let priceBox = document.createElement('div');
+					priceBox.classList.add('product-priceBox');
+					let productPrice = document.createElement('p');
+					productPrice.className = 'product-price';
+					productPrice.textContent = priceInput.textContent;
+					priceBox.appendChild(productPrice);
+
+					let removeButton = document.createElement('button');
+					removeButton.classList.add('product-removeBtn');
+					removeButton.textContent = '-';
+					removeButton.addEventListener('click', (ab) => {
+						const elementToRemove = document.querySelector(
+							`[product-index="${ix}"]`
+						);
+						if (elementToRemove) {
+							elementToRemove.remove();
+						}
+						urlList[ix] = null;
+					});
+					priceBox.appendChild(removeButton);
+					newDiv.appendChild(priceBox);
+					document.querySelector('.prices').appendChild(newDiv);
+				}
 			})
 			.catch((error) => console.error(error));
 	}
@@ -70,6 +75,7 @@ let urlList = [
 	'https://www.rohlik.cz/1367691-pernerka-mouka-psenicna-polohruba',
 	'https://www.rohlik.cz/1302745-lindt-excellence-horka-cokolada-s-morskou-soli',
 	'https://www.rohlik.cz/1342937-bumbu-original-craft',
+	'https://www.rohlik.cz/1407202-royal-crown-cola-no-sugar-plech',
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
