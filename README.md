e228e9594fedc3f5757007823c738c517dfcad29
Tomáš Kelbasa <tomas.plqa@gmail.com>
2023-09-15 14:52:10 +0200

    remove button added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Rohlik-Chrome-Extension
+
 Extension for Chrome which allows you to view current prices of products listed on https://rohlik.cz/ in one small popup.
--- main.js
+++ main.js
@@ -1,39 +1,59 @@
 function generateProduct(url, ix) {
-	fetch(url)
-		.then((response) => response.text())
-		.then((data) => {
-			//converts only body of fetched HTML to HTML
-			let dc = new DOMParser().parseFromString(
-				data.split('<body>')[1],
-				'text/html'
-			);
-			let priceInput = dc.querySelector('[data-test="product-price"');
-			if (priceInput.querySelector('span') !== null) {
-				priceInput.removeChild(priceInput.querySelector('span'));
-			}
+	if (url !== null) {
+		fetch(url)
+			.then((response) => response.text())
+			.then((data) => {
+				//converts only body of fetched HTML to HTML
+				let dc = new DOMParser().parseFromString(
+					data.split('<body>')[1],
+					'text/html'
+				);
+				let priceInput = dc.querySelector('[data-test="product-price"');
+				if (priceInput.querySelector('span') !== null) {
+					priceInput.removeChild(priceInput.querySelector('span'));
+				}
 
-			let nameInput = dc.querySelector('h2');
+				let nameInput = dc.querySelector('h2');
 
-			let detailInput = dc.querySelector('.detailQuantity');
+				let detailInput = dc.querySelector('.detailQuantity');
 
-			let newDiv = document.createElement('div');
-			newDiv.classList.add('product-box');
-			newDiv.setAttribute('product-index', ix);
-			let productName = document.createElement('a');
-			productName.className = 'product-name';
-			productName.href = url;
-			productName.target = "_blank";
-			newDiv.appendChild(productName);
-			newDiv.querySelector('.product-name').textContent =
-				nameInput.textContent + ' - ' + detailInput?.textContent;
-			let productPrice = document.createElement('p');
-			productPrice.className = 'product-price';
-			newDiv.appendChild(productPrice);
-			newDiv.querySelector('.product-price').textContent =
-				priceInput.textContent;
-			document.querySelector('.prices').appendChild(newDiv);
-		})
-		.catch((error) => console.error(error));
+				let newDiv = document.createElement('div');
+				newDiv.classList.add('product-box');
+				newDiv.setAttribute('product-index', ix);
+
+				let productName = document.createElement('a');
+				productName.className = 'product-name';
+				productName.href = url;
+				productName.target = '_blank';
+				newDiv.appendChild(productName);
+				newDiv.querySelector('.product-name').textContent =
+					nameInput.textContent + ' - ' + detailInput?.textContent;
+
+				let priceBox = document.createElement('div');
+				priceBox.classList.add("product-priceBox");
+				let productPrice = document.createElement('p');
+				productPrice.className = 'product-price';
+				productPrice.textContent = priceInput.textContent;
+				priceBox.appendChild(productPrice);
+
+				let removeButton = document.createElement('button');
+				removeButton.classList.add('product-removeBtn');
+				removeButton.textContent = '-';
+				removeButton.addEventListener('click', (ab) => {
+					const elementToRemove = document.querySelector(
+						`[product-index="${ix}"]`
+					);
+					if (elementToRemove) {
+						elementToRemove.remove();
+					}
+					urlList[ix] = null;
+				});
+				priceBox.appendChild(removeButton);
+				newDiv.appendChild(priceBox);
+				document.querySelector('.prices').appendChild(newDiv);
+			})
+			.catch((error) => console.error(error));
+	}
 }
 
 let urlList = [
@@ -51,4 +71,6 @@ let urlList = [
 	'https://www.rohlik.cz/1342937-bumbu-original-craft',
 ];
 
-urlList.map((url, ix) => generateProduct(url, ix));
+document.addEventListener('DOMContentLoaded', () => {
+	urlList.map((url, ix) => generateProduct(url, ix));
+});
