92cc63acea94ae3a37aae323aa4685163b6da42d
TomasKelbasa <tomas.plqa@gmail.com>
2023-09-28 17:05:23 +0200

    async hell

--- main.js
+++ main.js
@@ -1,39 +1,60 @@
 import fetchFromURL from "./rohlik_fetcher.js";
+
 async function generateProduct(url, ix) {
-	
+	await fetchFromURL(url).then((d) => {
+		console.log(d.name);
+
+		let product = document.createElement('div');
+		product.classList.add("product-box");
+		product.textContent = d.name;
+
+		let removeButton = document.createElement('button');
+		removeButton.classList.add('product-removeBtn');
+		removeButton.textContent = '-';
+		removeButton.addEventListener('click', (ab) => {
+			loadURLList((urlList) => {
+				urlList[ix] = 'X';
+				saveURLList(urlList);
+			});
+			document.querySelector(
+				`[product-index="${ix}"]`
+			)?.remove();
+			
+		});
+		return product;
+	});
+
+
+
+
+
 }
 
 let productsEl = document.createElement('div');
 
 async function generateProducts(products, callback) {
+	let result = [];
+	for(let i = 0; i < products.length; i++){
+		result.push(await generateProduct(products[i], i));
+	}
+	return result;
+	/*
 	let promises = products.map(async (url, ix) => {
 			await generateProduct(url, ix);
 	});
 	await Promise.all(promises);
 	callback();
+	*/
 }
 
 
 function listBuilder(){
 	/*
-	let removeButton = document.createElement('button');
-					removeButton.classList.add('product-removeBtn');
-					removeButton.textContent = '-';
-					removeButton.addEventListener('click', (ab) => {
-						loadURLList((urlList) => {
-							urlList[ix] = 'X';
-							saveURLList(urlList);
-						});
-						const elementToRemove = document.querySelector(
-							`[product-index="${ix}"]`
-						);
-						if (elementToRemove) {
-							elementToRemove.remove();
-						}
-					});*/
-	fetchFromURL("https://www.rohlik.cz/1406203-miil-cerstve-mleko-polotucne-1-5-tuku").then((data) => {
+		fetchFromURL("https://www.rohlik.cz/1406203-miil-cerstve-mleko-polotucne-1-5-tuku").then((data) => {
 		console.log(data);
 	});
+*/
+
 	
 }
 
@@ -46,7 +67,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		if (Array.isArray(ul)) {
 			ul = ul.filter((el) => el != null && el != 'null' && el != 'X');
 			saveURLList(ul);
-			generateProducts(ul, async () => {
+			generateProducts(ul).then((result)=> console.log(result));/*, async () => {
 				let parentN = document.querySelector('.prices');
 
 				let divs = Array.from(productsEl.children);
@@ -67,8 +88,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 					msg.textContent = "No products on the list";
 					parentN.appendChild(msg);
 				}
-
-			});
+				
+			});*/
 		}
 	});
 });
